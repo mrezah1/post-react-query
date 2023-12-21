09851fa26b9da56872ee61413e8968ad13077682
Mohammadreza <mrhhajiyan@gmail.com>
2023-12-21 03:38:42 +0330

    input general component

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Field, useForm, useWatch } from "rc-field-form";
+
+interface InputProps {
+  label: string;
+  name: string;
+  type?: string;
+  required?: boolean;
+  isArea?: boolean;
+  validations?: object[];
+  err: string;
+}
+
+const MyInput: React.FC<InputProps> = ({
+  name,
+  label,
+  required = true,
+  type = "text",
+  err,
+  isArea = false,
+  validations = [],
+  ...rest
+}) => {
+  return (
+    <Field
+      name={name}
+      rules={[
+        { required: required, message: `${name} is required!` },
+        ...validations,
+      ]}
+    >
+      {console.log(validations)}
+      <div className="input-item">
+        <label htmlFor={name}>{label}</label>
+        {isArea ? (
+          <textarea
+            id={name}
+            {...rest}
+            {...(err ? { style: { borderColor: "#f84f4f" } } : {})}
+          ></textarea>
+        ) : (
+          <input
+            {...(err ? { style: { borderColor: "#f84f4f" } } : {})}
+            id={name}
+            type={type}
+            {...rest}
+          />
+        )}
+        {err && <p style={{ color: "#f84f4f" }}>{err}</p>}
+      </div>
+    </Field>
+  );
+};
+export default MyInput;
