a59bcfb43558cbe72933f54e4aa76faf7a3764d8
Mohammadreza <mrhhajiyan@gmail.com>
2023-12-21 03:34:52 +0330

    initialize project files

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Post from "./components/Posts/Post";
+import { Wrapper } from "./components/shared/Card";
+import "./App.scss";
+import { ToastBar, Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  },
+});
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Wrapper>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="/posts/:id" element={<Post />} />
+            </Route>
+          </Routes>
+        </Router>
+        <Toaster position="bottom-right"/>
+      </Wrapper>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
