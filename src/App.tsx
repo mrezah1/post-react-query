e2c5c5c21ccf08afe6cdeca1c459c7d17a46f17e
MrezaH <49529160+mrezah1@users.noreply.github.com>
2024-02-04 19:01:49 +0330

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Home from "./components/Home";
 import Post from "./components/Posts/Post";
 import { Wrapper } from "./components/shared/Card";
 import "./App.scss";
-import { ToastBar, Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,16 +19,16 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Wrapper>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}>
-              <Route path="/posts/:id" element={<Post />} />
-            </Route>
-          </Routes>
-        </Router>
-        <Toaster position="bottom-right"/>
-      </Wrapper>
+        <Wrapper>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />}>
+                <Route path="/posts/:id" element={<Post />} />
+              </Route>
+            </Routes>
+          </Router>
+          <Toaster position="bottom-right" />
+        </Wrapper>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
